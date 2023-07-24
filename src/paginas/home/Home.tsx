aabd01c8754b6f1d93378c847cf8b18068c1617a
Micael Dos Santos <micael.augustons@hotmail.com>
2023-07-24 09:50:09 -0300

    Primeiro commit

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+
+import './Home.css';
+
+function Home() {
+    return(
+        <>
+            <h1 className='titulo'>Home</h1>
+            <img src="https://www.newlyswissed.com/wp-content/uploads/2023/01/Switzerland-Train-Map-Lago-Bianco-247127244-01b.jpg" alt="Imagem Tela Inicial" className='img'/>
+        </>
+    );
+}
+
+export default Home;
