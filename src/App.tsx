52ebad917d047836125c3b03eb86dac830862e8b
Micael Dos Santos <micael.augustons@hotmail.com>
2023-07-25 21:27:25 -0300

    Tailwind e Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import Home from './paginas/home/Home';
 import './App.css';
-import { useState } from 'react';
+import Home from './paginas/home/Home';
+import Navbar from './components/navBar/NavBar';
+import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './paginas/login/Login';
 
 function App() {
-  const [valor, setValor] = useState(0);
-
-  function handleClick() {
-    setValor(valor + 1);
-  }
   return (
-    //  <Home  title="Componente Home"
-    //         description="Este é um componente Home que recebe props."/>
-    <div>
-      <>
-        <Home/>
-      </>
-      <p>O valor é: {valor}</p>
-      <button onClick={handleClick}>Adicionar 1</button>
-    </div>
-  );
+    <>
+    <BrowserRouter>
+        <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
+);
 }
-
 export default App;
