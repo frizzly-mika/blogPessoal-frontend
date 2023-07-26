69296d822b1ac4c3351a2aebac944b68da68c90a
Micael Dos Santos <micael.augustons@hotmail.com>
2023-07-26 14:52:08 -0300

    Consumo backend com Axios, montagem Services, controle de Estado com Context API

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,3 +1,5 @@
+import { Link, useNavigate } from 'react-router-dom'
+
 
 function Navbar() {
  
@@ -10,6 +12,8 @@ function Navbar() {
             <div className='text-2xl font-bold uppercase'>Blog Pessoal</div>
 
             <div className='flex gap-4'>
+              <Link to='/login' className='hover:underline'>Login</Link>
+              <Link to='/home' className='hover:underline'>Home</Link>
               <div className='hover:underline'>Postagens</div>
               <div className='hover:underline'>Temas</div>
               <div className='hover:underline'>Cadastrar tema</div>
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,7 +1,6 @@
 import homeLogo from '../../assets/home.png'
 import './Home.css';
 
-
 function Home() {
     return (
         <>
@@ -18,12 +17,12 @@ function Home() {
             </div>
   
             <div className="flex justify-center ">
-              <img src="" alt="" className='w-2/3' />
+              <img src={homeLogo} alt="" className='w-2/3' />
       
             </div>
           </div>
         </div>
-      
+        
       </>
     );
 }
